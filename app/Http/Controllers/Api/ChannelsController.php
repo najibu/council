0986f8cc4ff0591ca42cd050227c8863e779df18
Najibu Nsubuga <1o1jibby@gmail.com>
2019-01-06 09:47:38 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Api/ChannelsController.php
+++ app/Http/Controllers/Api/ChannelsController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Channel;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class ChannelsController extends Controller
 {
     /**
-     * Fetch all channels
+     * Fetch all channels.
      */
     public function index()
     {
