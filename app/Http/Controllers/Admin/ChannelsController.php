cb67ec4919e2c66268f2154b16425693a5a27c9f
Najibu Nsubuga <ns.najibu@gmail.com>
2019-01-12 09:55:59 +0000

    Use withCount to optimise queries

--- app/Http/Controllers/Admin/ChannelsController.php
+++ app/Http/Controllers/Admin/ChannelsController.php
@@ -13,7 +13,7 @@ class ChannelsController extends Controller
     public function index()
     {
         $channels = Channel::withoutGlobalScopes()
-            ->with('threads')
+            ->withCount('threads')
             ->get();
 
         return view('admin.channels.index', compact('channels'));
--- resources/views/admin/channels/index.blade.php
+++ resources/views/admin/channels/index.blade.php
@@ -23,7 +23,7 @@
                     <td>{{$channel->name}}</td>
                     <td>{{$channel->slug}}</td>
                     <td>{{$channel->description}}</td>
-                    <td>{{ count($channel->threads) }}</td>
+                    <td>{{ $channel->threads_count }}</td>
                     <td>
                         <a href="{{ route('admin.channels.edit', ['channel' => $channel->slug]) }}"
                             class="btn btn-default btn-xs"
