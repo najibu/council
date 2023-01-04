ca7fd3468ccc823fbc294445a68ae3fd71152c84
Najibu Nsubuga <ns.najibu@gmail.com>
2023-01-04 14:34:26 +0000

    Apply fixes from StyleCI (#76)

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
 use App\Activity;
+use App\User;
 
 class ProfilesController extends Controller
 {
     /**
      * Show the user's profile.
      *
-     * @param  User $user
+     * @param  User  $user
      * @return \Response
      */
     public function show(User $user)
